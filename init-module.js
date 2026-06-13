97c9982ad25960328171c519327c235ba7319f44
mewad1945 <mikael.raza2012@gmail.com>
2026-06-13 10:20:58 +0200

    Refactor initialization logging and event dispatch

--- init-module.js
+++ init-module.js
@@ -1,9 +1,8 @@
+console.log("INIT-MODULE: Laddad");
 window.Module = {
     noInitialRun: true,
     onRuntimeInitialized: function() {
-        console.log("Wasm Runtime redo!");
-        // Skicka signal till script.js att biblioteket är redo att användas
-        window.dispatchEvent(new CustomEvent('moduleReady', { detail: window.Module }));
-    },
-    print: (msg) => console.log("XMRig:", msg)
+        console.log("WASM: Runtime redo");
+        window.dispatchEvent(new CustomEvent('moduleReady'));
+    }
 };

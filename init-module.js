9db1a5c51df612750719b6330c509c69714e6c3a
mewad1945 <mikael.raza2012@gmail.com>
2026-06-13 10:30:49 +0200

    Fix formatting and ensure miner readiness flag is set

--- init-module.js
+++ init-module.js
@@ -2,6 +2,6 @@ window.Module = {
     noInitialRun: true,
     onRuntimeInitialized: function() {
         console.log("Wasm Runtime redo!");
-        window.isMinerReady = true; // Flagga som script.js kommer leta efter
+        window.isMinerReady = true; 
     }
 };

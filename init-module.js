8ef05aa76c909be25f28c735b14f6ed7548aa3e0
mewad1945 <mikael.raza2012@gmail.com>
2026-06-13 10:39:18 +0200

    Update print and printErr methods for logging

--- init-module.js
+++ init-module.js
@@ -1,7 +1,8 @@
 window.Module = {
     noInitialRun: true,
-    print: (m) => console.log("Wasm Log:", m),
-    printErr: (m) => console.error("Wasm Error:", m),
+    // Lägg till dessa två rader för att fånga upp ALLT
+    print: (m) => console.log("Wasm-Output:", m),
+    printErr: (m) => console.error("Wasm-Error:", m),
     onRuntimeInitialized: function() {
         console.log("Wasm Runtime redo!");
         window.dispatchEvent(new Event('minerReady'));

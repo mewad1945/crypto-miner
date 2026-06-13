62481ab8d28301ef2bd1d4e03a036f402440110f
mewad1945 <mikael.raza2012@gmail.com>
2026-06-13 10:25:15 +0200

    Refactor Module initialization and logging

--- init-module.js
+++ init-module.js
@@ -1,8 +1,6 @@
-console.log("INIT-MODULE: Laddad");
 window.Module = {
-    noInitialRun: true,
     onRuntimeInitialized: function() {
-        console.log("WASM: Runtime redo");
-        window.dispatchEvent(new CustomEvent('moduleReady'));
-    }
+        console.log("Wasm Runtime redo!");
+    },
+    noInitialRun: true
 };
